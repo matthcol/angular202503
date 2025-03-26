0ecbf485b8415ca85c670384c33a08e1841e34cd
matthcol <matthias.colin@gmail.com>
2025-03-26 11:04:48 +0100

    interface CityFr

---
+++ city-app/src/app/interface/city-fr.ts
@@ -0,0 +1,10 @@
+export interface CityFr {
+    id?: string
+    code_insee?: string
+    code_postal?: string
+    nom_standard: string
+    dep_code?: string
+    population?: number
+    latitude_centre?: number
+    longitude_centre?: number
+}
